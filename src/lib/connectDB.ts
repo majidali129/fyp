7adc10d39238be4e173f4595c1b3fca9a1b7e113
Majid Ali <lahmajidali@gmail.com>
2024-08-17 07:38:27 +0500

    edit next config

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -2,10 +2,14 @@ import mongoose, { Connection } from "mongoose";
 
 let connection: Connection | null = null;
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) throw new Error("MONGODB_URI not defined");
+
 export const connectDB = async () => {
   if (connection) {
     console.log("DB is already connected ✅");
-    return;
+    return connection;
   }
 
   try {
