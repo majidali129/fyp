7f878f097ef3e0a05139f78ae86e3b880d874c28
Majid Ali <lahmajidali@gmail.com>
2024-08-17 08:08:21 +0500

    updated middleware file to fix runtime issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <Providers>
-      <SessionProvider>
-        <html lang="en">
-          <body className={inter.className}>{children}</body>
-        </html>
-      </SessionProvider>
-    </Providers>
+    <SessionProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -18,4 +18,17 @@ export default auth((req) => {
 
 export const config = {
   matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/verify/:path*"],
+  unstable_allowDynamic: [
+    "/node_modules/@nextui-org/calendar/dist/chunk-NABLCSM5.mjs",
+    "/node_modules/@nextui-org/theme/dist/chunk-YSA7EQBH.mjs",
+    "/node_modules/@nextui-org/calendar/dist/index.mjs",
+    "/node_modules/@nextui-org/react/dist/index.mjs",
+    "/node_modules/@nextui-org/theme/dist/index.mjs",
+    "/node_modules/lodash.kebabcase/index.js",
+    "/node_modules/lodash.debounce/index.js",
+    "/node_modules/lodash.mapkeys/index.js",
+    "/node_modules/lodash.omit/index.js",
+    "/node_modules/lodash.get/index.js",
+    "/node_modules/mongoose/dist/browser.umd.js"
+  ]
 };
