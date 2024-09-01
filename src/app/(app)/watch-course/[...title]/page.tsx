63c83dd0e500bc11aa184d5fdd2ea69c8e5a5e2a
Majid Ali <lahmajidali@gmail.com>
2024-09-01 14:53:07 +0500

    updated curriculum for mobile ui

--- src/app/(app)/watch-course/[...title]/page.tsx
+++ src/app/(app)/watch-course/[...title]/page.tsx
@@ -6,34 +6,42 @@ import { MdOutlinePlayCircle } from "react-icons/md";
 import { HiArrowLeft } from "react-icons/hi2";
 import { Progress } from "@/components/ui/progress";
 import CourseCurriculum from "@/components/CourseCurriculum";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const WatchCoursePage = () => {
   return (
-    <>
+    <section className="pb-10">
       <div className="py-4 px-6 bg-secondary-50 md:flex-between hidden ">
         <div className="flex-start !gap-x-3">
-        <div>
-            <Button size={'icon'} variant={'transparent'} className="rounded-full hover:ring-1 hover:ring-gray-400">
-                <HiArrowLeft className="w-5 h-5" />
+          <div>
+            <Button
+              size={"icon"}
+              variant={"transparent"}
+              className="rounded-full hover:ring-1 hover:ring-gray-400"
+            >
+              <HiArrowLeft className="w-5 h-5" />
             </Button>
-        </div>
-        <div>
-            <h5>Complete Website Responsive Design: from Figma to Webflow to Website Design</h5>
+          </div>
+          <div>
+            <h5>
+              Complete Website Responsive Design: from Figma to Webflow to
+              Website Design
+            </h5>
             <div className="flex-start !gap-x-3.5">
-                  <div className="flex-start">
-                    <FaRegFolderOpen className="text-primary-500 w-4 h-4" />
-                    <span>{"6"} Sections</span>
-                  </div>
-                  <div className="flex-start">
-                    <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
-                    <span>202 Lectures</span>
-                  </div>
-                  <div className="flex-start">
-                    <IoTimeOutline className="text-warning-500 w-4 h-4" />
-                    <span>19h 37m</span>
-                  </div>
-                </div>
-        </div>
+              <div className="flex-start">
+                <FaRegFolderOpen className="text-primary-500 w-4 h-4" />
+                <span>{"6"} Sections</span>
+              </div>
+              <div className="flex-start">
+                <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
+                <span>202 Lectures</span>
+              </div>
+              <div className="flex-start">
+                <IoTimeOutline className="text-warning-500 w-4 h-4" />
+                <span>19h 37m</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="flex-center !gap-x-3">
           <Button
@@ -43,30 +51,66 @@ const WatchCoursePage = () => {
           >
             Write A Reveiw
           </Button>
-          <Button size={'sm'}>Next Lecture</Button>
+          <Button size={"sm"}>Next Lecture</Button>
         </div>
       </div>
 
-      <section className="p-6 ">
-        <div className="grid md:grid-cols-[1fr_38%]  md:gap-x-3">
-        <div className="border">
+      <section className="md:p-6">
+        <div className="grid grid-cols-1 md:grid-cols-[1fr_38%]  md:gap-x-3">
+          <div className="border">
             <div>Player</div>
-        </div>
-        <div className="space-y-3">
+          </div>
+          {/* course curriculum */}
+          <div className="space-y-3 hidden md:block">
             <div className="space-y-1.5">
-                <div className="flex-between">
-                    <h5>Course Contents</h5>
-                    {/* //TODO: show user how much course get completed */}
-                    <h6 className="text-success-500">{'15'}% Completed</h6>
-                </div>
-                <Progress className="h-1 rounded-none bg-gray-100 *:bg-success-500" />
+              <div className="flex-between">
+                <h5>Course Contents</h5>
+                {/* //TODO: show user how much course get completed */}
+                <h6 className="text-success-500">{"15"}% Completed</h6>
+              </div>
+              <Progress className="h-1 rounded-none bg-gray-100 *:bg-success-500" />
             </div>
-            <CourseCurriculum curriculum={[]}  isWatching={true}/>
-        </div>
+            <CourseCurriculum curriculum={[]} isWatching={true} />
+          </div>
+
+          {/* course metadata */}
+          <Tabs className="*:px-3" defaultValue="overview">
+            <TabsList className="border-y border-y-gray-200 py-2 md:py-2.5 *:md:font-semibold *:md:tracking-wide *:md:text-gray-800 md:flex-start !flex-between !px-0">
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="content" className="md:hidden">
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger value="notes">Notes</TabsTrigger>
+              <TabsTrigger value="reviews">Reviews</TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview">
+              <CourseDescription description="" />
+            </TabsContent>
+            <TabsContent value="content" className="md:hidden">
+              {/* Curriculum */}
+              <CourseCurriculum isWatching={true} curriculum={[]} />
+            </TabsContent>
+            <TabsContent value="notes">
+              <LectureNotes notes={[""]} />
+            </TabsContent>
+            <TabsContent value="reviews">
+              <LectureReviews reviews={[""]} />
+            </TabsContent>
+          </Tabs>
         </div>
       </section>
-    </>
+    </section>
   );
 };
 
 export default WatchCoursePage;
+
+function CourseDescription({ description }: { description: string }) {
+  return "";
+}
+function LectureNotes({ notes }: { notes: [any] }) {
+  return "";
+}
+function LectureReviews({ reviews }: { reviews: [any] }) {
+  return "";
+}
--- src/components/CourseCurriculum.tsx
+++ src/components/CourseCurriculum.tsx
@@ -14,34 +14,42 @@ interface CurriculumProps {
   isWatching?: boolean;
 }
 
-const CourseCurriculum = ({
-  curriculum,
-  isWatching
-}: CurriculumProps) => {
+const CourseCurriculum = ({ curriculum, isWatching }: CurriculumProps) => {
   return (
     <div className=" border border-gray-100 py-0 flex items-start gap-x-5 rounded-sm">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger className="[&>div]:relative [&>div>svg]:absolute *:py-3 [&>div]:px-3 [&>div]:ps-7 [&>div>svg]:left-1 [&>div>svg]:top-1/2 [&>div>svg]:-translate-y-1/2 [&[data-state=open]>div>div>h6]:text-primary-500  ">
+          <AccordionTrigger className="[&>div]:relative [&>div>svg]:absolute *:py-3 [&>div]:px-3 [&>div]:ps-7 [&>div>svg]:left-1 [&>div>svg]:top-1/2 [&>div>svg]:-translate-y-1/2 [&[data-state=open]>div>div>div>div>h6]:text-primary-500  ">
             <div className="flex-between w-full">
               {/* //TODO: show finish status to user */}
-              <h6 className="text-gray-800">{"Geting Started"}</h6>
-              <div className="flex-between !gap-x-2">
-              <div className="flex-start !gap-x-3">
-                <div className="flex-start !gap-x-1">
-                  <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
-                  <span className="text-[.8rem]">{"8 Lectures"}</span>
+              <div className="w-full text-start">
+                <div className="flex-between">
+                  <h6 className="text-gray-800">{"Geting Started"}</h6>
+                  <div className="flex-start !gap-x-3">
+                    <div className="flex-start !gap-x-1">
+                      <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
+                      <span className="text-[.8rem]">{"8 Lectures"}</span>
+                    </div>
+                    <div className="flex-start !gap-x-1">
+                      <IoTimeOutline className="text-warning-500 w-4 h-4" />
+                      <span className="text-[.8rem]">{"37m"}</span>
+                    </div>
+                    {isWatching && (
+                      <div className="flex items-center gap-x-0.5 max-sm:hidden">
+                        <BiCheckDouble className="w-4 h-4 text-success-500" />
+                        <span>25% finish</span>
+                        <span className="text-gray-500">(1/4)</span>
+                      </div>
+                    )}
+                  </div>
                 </div>
-                <div className="flex-start !gap-x-1">
-                  <IoTimeOutline className="text-warning-500 w-4 h-4" />
-                  <span className="text-[.8rem]">{"37m"}</span>
-                </div>
-              </div>
-              {isWatching && <div className="flex items-center gap-x-0.5">
-                <BiCheckDouble className="w-4 h-4 text-success-500" />
-                <span>25% finish</span>
-                <span className="text-gray-500">(1/4)</span>
-                </div>}
+                {isWatching && (
+                  <div className="flex items-center gap-x-0.5 md:hidden">
+                    <BiCheckDouble className="w-4 h-4 text-success-500" />
+                    <span>25% finish</span>
+                    <span className="text-gray-500">(1/4)</span>
+                  </div>
+                )}
               </div>
             </div>
           </AccordionTrigger>
@@ -51,26 +59,37 @@ const CourseCurriculum = ({
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger className="[&>div]:relative [&>div>svg]:absolute *:py-3 [&>div]:px-3 [&>div]:ps-7 [&>div>svg]:left-1 [&>div>svg]:top-1/2 [&>div>svg]:-translate-y-1/2 [&[data-state=open]>div>div>h6]:text-primary-500  ">
+          <AccordionTrigger className="[&>div]:relative [&>div>svg]:absolute *:py-3 [&>div]:px-3 [&>div]:ps-7 [&>div>svg]:left-1 [&>div>svg]:top-1/2 [&>div>svg]:-translate-y-1/2 [&[data-state=open]>div>div>div>div>h6]:text-primary-500  ">
             <div className="flex-between w-full">
               {/* //TODO: show finish status to user */}
-              <h6 className="text-gray-800">{"Geting Started"}</h6>
-              <div className="flex-between !gap-x-2">
-              <div className="flex-start !gap-x-3">
-                <div className="flex-start !gap-x-1">
-                  <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
-                  <span className="text-[.8rem]">{"8 Lectures"}</span>
+              <div className="w-full text-start">
+                <div className="flex-between">
+                  <h6 className="text-gray-800">{"Geting Started"}</h6>
+                  <div className="flex-start !gap-x-3">
+                    <div className="flex-start !gap-x-1">
+                      <MdOutlinePlayCircle className="text-secondary-500 w-4 h-4" />
+                      <span className="text-[.8rem]">{"8 Lectures"}</span>
+                    </div>
+                    <div className="flex-start !gap-x-1">
+                      <IoTimeOutline className="text-warning-500 w-4 h-4" />
+                      <span className="text-[.8rem]">{"37m"}</span>
+                    </div>
+                    {isWatching && (
+                      <div className="flex items-center gap-x-0.5 max-sm:hidden">
+                        <BiCheckDouble className="w-4 h-4 text-success-500" />
+                        <span>25% finish</span>
+                        <span className="text-gray-500">(1/4)</span>
+                      </div>
+                    )}
+                  </div>
                 </div>
-                <div className="flex-start !gap-x-1">
-                  <IoTimeOutline className="text-warning-500 w-4 h-4" />
-                  <span className="text-[.8rem]">{"37m"}</span>
-                </div>
-              </div>
-              {isWatching && <div className="flex items-center gap-x-0.5">
-                <BiCheckDouble className="w-4 h-4 text-success-500" />
-                <span>25% finish</span>
-                <span className="text-gray-500">(1/4)</span>
-                </div>}
+                {isWatching && (
+                  <div className="flex items-center gap-x-0.5 md:hidden">
+                    <BiCheckDouble className="w-4 h-4 text-success-500" />
+                    <span>25% finish</span>
+                    <span className="text-gray-500">(1/4)</span>
+                  </div>
+                )}
               </div>
             </div>
           </AccordionTrigger>
@@ -79,6 +98,7 @@ const CourseCurriculum = ({
             <CourseLecture isWatching={isWatching!} lecture="" />
           </AccordionContent>
         </AccordionItem>
+
       </Accordion>
     </div>
   );
