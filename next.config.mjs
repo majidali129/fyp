e2a1a22671f734972297a5138e8a0f218bb55ea6
majidali129 <itsmajidali129@gmail.com>
2024-12-30 12:46:00 +0500

    added cors

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,36 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'images.pexels.com',
-            port: ''
-        },
-        {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            port: ''
-        }
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://fyp-alpha-virid.vercel.app/:path*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
