424664bee501304e771c2d6019ddf86a5235c149
majidali129 <itsmajidali129@gmail.com>
2024-12-30 14:15:41 +0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,10 @@ const nextConfig = {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
           },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          }
         ],
       },
     ];
