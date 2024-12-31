7e413b173f85251ce3a3fd3f56d6d2eab21a15b5
majidali129 <itsmajidali129@gmail.com>
2024-12-31 23:50:08 +0500

    removed time limit from axios client

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -5,7 +5,6 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: "http://localhost:3000/api",
-    timeout: 10000,
     withCredentials: true,
     httpsAgent: process.env.NODE_ENV === 'production'? true: false,
     headers: {
